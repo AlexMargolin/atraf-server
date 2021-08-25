d3a2c077604719d0e21d7ec042a215b0b6fa93f5
Alex Margolin <csight@msn.com>
2021-08-25 04:12:07 +0300

    [main] mysql database connection

--- cmd/authentication/main.go
+++ cmd/authentication/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"net"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
 
 	"quotes/config"
 )
 
 type Config struct {
 	DbHost       string `env:"MYSQL_HOST"`     // Database host name
-	DbPort       int    `env:"MYSQL_PORT"`     // Database port
+	DbPort       string `env:"MYSQL_PORT"`     // Database port
 	DbUser       string `env:"MYSQL_USER"`     // Database username
 	DbPass       string `env:"MYSQL_PASS"`     // Database user password
 	DbName       string `env:"MYSQL_NAME"`     // Database DB Name
-	ServerPort   int    `env:"SERVER_PORT"`    // Server Port
+	ServerPort   string `env:"SERVER_PORT"`    // Server Port
 	AuthATSecret string `env:"AUTH_AT_SECRET"` // Authentication Access Token Signing Secret
 	AuthRTSecret string `env:"AUTH_RT_SECRET"` // Authentication Refresh Token Signing Secret
 }
@@ -21,11 +26,11 @@ func main() {
 	// Default Configuration
 	cfg := &Config{
 		DbHost:       "localhost",
-		DbPort:       3306,
+		DbPort:       "3306",
 		DbUser:       "root",
 		DbPass:       "",
 		DbName:       "at_db",
-		ServerPort:   8080,
+		ServerPort:   "8080",
 		AuthATSecret: "abcd",
 		AuthRTSecret: "dcba",
 	}
@@ -33,4 +38,41 @@ func main() {
 	if err := config.Marshal(cfg); err != nil {
 		log.Fatal(err)
 	}
+
+	// MySQL Database connection
+	_, err := NewDatabaseConnection(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// NewDatabaseConnection Returns a new MySql DB Connection or error on failure
+func NewDatabaseConnection(c *Config) (*sql.DB, error) {
+	// DSN Configuration
+	mysqlConfig := &mysql.Config{
+		Addr:                 net.JoinHostPort(c.DbHost, c.DbPort),
+		User:                 c.DbUser,
+		Passwd:               c.DbPass,
+		DBName:               c.DbName,
+		AllowNativePasswords: true,
+	}
+
+	// DSN config string
+	dsn := mysqlConfig.FormatDSN()
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the DB and make sure the server is reachable
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(time.Minute * 3)
+
+	return db, nil
 }
